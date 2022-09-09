47d47b630e1e691e8a11ef74936d68bba25aea58
mrocznaczarownica <mr0cznaczar0wn1caa@gmail.com>
2022-09-09 21:20:25 +0300

    a

---
+++ app/src/main/java/com/example/medditation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.medditation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val timer = object :CountDownTimer(3000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@MainActivity, onboarding::class.java)
+
+                startActivity(intent)
+                finish()
+            }
+
+        }
+        timer.start()
+    }
+}
---
+++ app/src/main/java/com/example/medditation/onboarding.kt
@@ -0,0 +1,14 @@
+package com.example.medditation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class onboarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+
+    fun loginClick(view: View) {}
+}
